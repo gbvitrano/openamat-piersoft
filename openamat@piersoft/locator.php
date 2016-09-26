9564507aa3298289bad84f808b93066c7b8f42ba
gbvitrano <gbvitrano@gmail.com>
2016-09-26 20:58:16 +0200

    Webbapp di @piersoft personalizzata con i gtfs di Palermo  bug fix

--- openamat@piersoft/locator.php
+++ openamat@piersoft/locator.php
@@ -22,7 +22,7 @@ $r=$_GET["r"];
 <script src="http://turbo87.github.io/leaflet-sidebar/src/L.Control.Sidebar.js"></script>
    <script src="leaflet.markercluster.js"></script>
    <script src="http://joker-x.github.io/Leaflet.geoCSV/lib/jquery.js"></script>
-
+   
 <script type="text/javascript">
 
 function microAjax(B,A){this.bindFunction=function(E,D){return function(){return E.apply(D,[D])}};this.stateChange=function(D){if(this.request.readyState==4 ){this.callbackFunction(this.request.responseText)}};this.getRequest=function(){if(window.ActiveXObject){return new ActiveXObject("Microsoft.XMLHTTP")}else { if(window.XMLHttpRequest){return new XMLHttpRequest()}}return false};this.postBody=(arguments[2]||"");this.callbackFunction=A;this.url=B;this.request=this.getRequest();if(this.request){var C=this.request;C.onreadystatechange=this.bindFunction(this.stateChange,this);if(this.postBody!==""){C.open("POST",B,true);C.setRequestHeader("X-Requested-With","XMLHttpRequest");C.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8");C.setRequestHeader("Connection","close")}else{C.open("GET",B,true)}C.send(this.postBody)}};
